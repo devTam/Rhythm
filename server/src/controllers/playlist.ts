16b4592655df183419426bd347a3eea65e4c8288
devTam <tammy.batubo@gmail.com>
2024-03-07 16:55:58 +0100

    add get playlist with audios route

--- server/src/controllers/playlist.ts
+++ server/src/controllers/playlist.ts
@@ -1,7 +1,12 @@
 import Audio from "@/models/audio"
 import Playlist from "@/models/playlist"
-import { ICreatePlaylistRequest, IUpdatePlaylistRequest } from "@/types/audio"
+import {
+  ICreatePlaylistRequest,
+  IPopulatedFavList,
+  IUpdatePlaylistRequest,
+} from "@/types/audio"
 import { RequestHandler } from "express"
+import { isValidObjectId } from "mongoose"
 
 export const createPlaylist: RequestHandler = async (
   req: ICreatePlaylistRequest,
@@ -67,6 +72,113 @@ export const updatePlaylist: RequestHandler = async (
       id: playlist._id,
       title: playlist.title,
       visibility: playlist.visibility,
+    },
+  })
+}
+
+export const deletePlaylist: RequestHandler = async (req, res) => {
+  const { playlistId, resId, all } = req.query
+  const ownerId = req.user.id
+
+  if (!isValidObjectId(playlistId))
+    return res.status(422).json({ error: "Invalid playlist Id" })
+
+  if (all === "yes") {
+    const playlist = await Playlist.findByIdAndDelete({
+      _id: playlistId,
+      owner: ownerId,
+    })
+
+    if (!playlist)
+      return res.status(404).json({
+        error: "Playlist not found!",
+      })
+  }
+
+  if (resId) {
+    if (!isValidObjectId(resId))
+      return res.status(422).json({ error: "Invalid audio Id" })
+
+    const playlist = await Playlist.findOneAndUpdate(
+      {
+        _id: playlistId,
+        owner: ownerId,
+      },
+      {
+        $pull: { items: resId },
+      }
+    )
+    if (!playlist)
+      return res.status(404).json({
+        error: "Playlist not found!",
+      })
+  }
+  res.json({ success: true })
+}
+
+export const getPlaylistByProfile: RequestHandler = async (req, res) => {
+  const { pageNum = "0", limit = "20" } = req.query as {
+    pageNum: string
+    limit: string
+  }
+  const data = await Playlist.find({
+    owner: req.user.id,
+    visibility: { $ne: "auto" },
+  })
+    .skip(parseInt(pageNum) * parseInt(limit))
+    .limit(parseInt(limit))
+    .sort("-createdAt")
+
+  const playlist = data.map((item) => {
+    return {
+      id: item._id,
+      title: item.title,
+      audioCount: item.items.length,
+      visibility: item.visibility,
+    }
+  })
+
+  res.json({ playlist })
+}
+
+export const getAudios: RequestHandler = async (req, res) => {
+  const { playlistId } = req.params
+
+  if (!isValidObjectId(playlistId))
+    return res.status(422).json({ error: "Invalid playlist Id" })
+
+  const playlist = await Playlist.findOne({
+    owner: req.user.id,
+    _id: playlistId,
+  }).populate<{ items: IPopulatedFavList[] }>({
+    path: "items",
+    populate: {
+      path: "owner",
+      select: "name",
+    },
+  })
+
+  if (!playlist) return res.json({ list: [] })
+
+  const audios = playlist?.items.map((item) => {
+    return {
+      id: item._id,
+      title: item.title,
+      category: item.category,
+      file: item.file.url,
+      poster: item.poster?.url,
+      owner: {
+        name: item.owner.name,
+        id: item.owner._id,
+      },
+    }
+  })
+
+  res.json({
+    list: {
+      id: playlist._id,
+      title: playlist.title,
+      audios,
     },
   })
 }
--- server/src/routers/playlist.ts
+++ server/src/routers/playlist.ts
@@ -1,13 +1,34 @@
-import { createPlaylist, updatePlaylist } from "@/controllers/playlist";
-import { isAuth, isVerified } from "@/middlewares/auth";
-import { validate } from "@/middlewares/validator";
-import { NewPlaylistValidationSchema, PlaylistValidationSchema } from "@/utils/validationSchema";
-import { Router } from "express";
+import {
+  createPlaylist,
+  updatePlaylist,
+  deletePlaylist,
+  getPlaylistByProfile,
+  getAudios,
+} from "@/controllers/playlist"
+import { isAuth, isVerified } from "@/middlewares/auth"
+import { validate } from "@/middlewares/validator"
+import {
+  NewPlaylistValidationSchema,
+  PlaylistValidationSchema,
+} from "@/utils/validationSchema"
+import { Router } from "express"
 
-const router = Router();
+const router = Router()
 
-router.post("/create", isAuth, isVerified, validate(NewPlaylistValidationSchema), createPlaylist)
+router.post(
+  "/create",
+  isAuth,
+  isVerified,
+  validate(NewPlaylistValidationSchema),
+  createPlaylist
+)
 
 router.patch("/", isAuth, validate(PlaylistValidationSchema), updatePlaylist)
 
-export default router;
+router.delete("/", isAuth, deletePlaylist)
+
+router.get("/by-profile", isAuth, getPlaylistByProfile)
+
+router.get("/:playlistId", isAuth, getAudios)
+
+export default router
