eedd496fc48b1cd17728d60a20e5329b1853bc7f
devTam <tammy.batubo@gmail.com>
2024-03-12 22:12:42 +0100

    add get history and recent history routes

--- server/src/controllers/history.ts
+++ server/src/controllers/history.ts
@@ -1,4 +1,5 @@
 import History, { IHistoryType } from "@/models/history"
+import { IPaginationQuery } from "@/types/misc"
 import { RequestHandler } from "express"
 
 export const updateHistory: RequestHandler = async (req, res) => {
@@ -98,4 +99,132 @@ export const deleteHistory: RequestHandler = async (req, res) => {
   )
 
   res.json({ success: true })
+}
+
+export const getHistories: RequestHandler = async (req, res) => {
+  const { limit = "20", pageNum = "0" } = req.query as IPaginationQuery
+  const histories = await History.aggregate([
+    { $match: { owner: req.user.id } },
+    {
+      $project: {
+        all: {
+          $slice: [
+            "$all",
+            parseInt(limit) * parseInt(pageNum),
+            parseInt(limit),
+          ],
+        },
+      },
+    },
+    { $unwind: "$all" },
+    {
+      $lookup: {
+        from: "audios",
+        localField: "all.audio",
+        foreignField: "_id",
+        as: "audioInfo",
+      },
+    },
+    { $unwind: "$audioInfo" },
+    {
+      $project: {
+        _id: 0,
+        id: "$all._id",
+        audioId: "$audioInfo._id",
+        date: "$all.date",
+        title: "$audioInfo.title",
+      },
+    },
+    {
+      $group: {
+        _id: {
+          $dateToString: { format: "%Y-%m-%d", date: "$date" },
+        },
+        audios: { $push: "$$ROOT" },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        id: "$id",
+        date: "$_id",
+        audios: "$$ROOT.audios",
+      },
+    },
+    { $sort: { date: -1 } },
+  ])
+  res.json({ histories })
+}
+
+export const getRecentlyPlayed: RequestHandler = async (req, res) => {
+  const match = { $match: { owner: req.user.id } }
+  const sliceMatch = {
+    $project: {
+      myHistory: { $slice: ["$all", 2] },
+    },
+  }
+  const dateSort = {
+    $project: {
+      histories: {
+        $sortArray: {
+          input: "$myHistory",
+          sortBy: { date: -1 },
+        },
+      },
+    },
+  }
+  const unwindWithIndex = {
+    $unwind: { path: "$histories", includeArrayIndex: "index" },
+  }
+  const audioLookup = {
+    $lookup: {
+      from: "audios",
+      localField: "histories.audio",
+      foreignField: "_id",
+      as: "audioInfo",
+    },
+  }
+  const unwindAudioInfo = {
+    $unwind: "$audioInfo",
+  }
+  const userLookup = {
+    $lookup: {
+      from: "users",
+      localField: "audioInfo.owner",
+      foreignField: "_id",
+      as: "owner",
+    },
+  }
+
+  const unwindUser = {
+    $unwind: "$owner",
+  }
+
+  const projectResult = {
+    $project: {
+      _id: 0,
+      id: "$audioInfo._id",
+      title: "$audioInfo.title",
+      about: "$audioInfo.about",
+      poster: "$audioInfo.poster.url",
+      file: "$audioInfo.file.url",
+      category: "$audioInfo.category",
+      owner: { id: "$owner._id", name: "$owner.name" },
+      date: "$histories.date",
+      progress: "$histories.progress",
+    },
+  }
+
+  const audios = await History.aggregate([
+    match,
+    sliceMatch,
+    dateSort,
+    unwindWithIndex,
+    audioLookup,
+    unwindAudioInfo,
+    userLookup,
+    unwindUser,
+    projectResult,
+  ])
+  res.json({ audios })
 }
--- server/src/routers/history.ts
+++ server/src/routers/history.ts
@@ -1,4 +1,9 @@
-import { updateHistory, deleteHistory } from "@/controllers/history"
+import {
+  updateHistory,
+  deleteHistory,
+  getHistories,
+  getRecentlyPlayed,
+} from "@/controllers/history"
 import { isAuth } from "@/middlewares/auth"
 import { validate } from "@/middlewares/validator"
 import { updateHistoryValidationSchema } from "@/utils/validationSchema"
@@ -8,5 +13,7 @@ const router = Router()
 
 router.post("/", isAuth, validate(updateHistoryValidationSchema), updateHistory)
 router.delete("/", isAuth, deleteHistory)
+router.get("/", isAuth, getHistories)
+router.get("/recently-played", isAuth, getRecentlyPlayed)
 
 export default router
