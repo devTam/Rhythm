e1a63307c9c197249a25ad24be42d77a1b79db72
devTam <tammy.batubo@gmail.com>
2024-04-08 22:09:30 +0100

    setup redux

--- RhythmPlayer/App.tsx
+++ RhythmPlayer/App.tsx
@@ -1,11 +1,15 @@
 import {NavigationContainer} from '@react-navigation/native';
 import AuthNavigator from 'navigation/AuthNavigator';
+import {Provider} from 'react-redux';
+import store from 'store';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <AuthNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <AuthNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
