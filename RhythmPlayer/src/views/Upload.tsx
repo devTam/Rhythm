b4efd4415b37c53df2fe732b2390de6d362e883a
devTam <tammy.batubo@gmail.com>
2024-04-24 21:55:13 +0100

    file selector UI

--- RhythmPlayer/src/views/Upload.tsx
+++ RhythmPlayer/src/views/Upload.tsx
@@ -1,18 +1,48 @@
+import Selector from 'components/Selector';
 import {FC} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import colors from 'utils/colors';
 
 interface Props {}
 
 const Upload: FC<Props> = props => {
   return (
     <View style={styles.container}>
-      <Text style={{color: 'white', fontSize: 30}}>Upload</Text>
+      <View style={styles.fileSelectorContainer}>
+        <Selector
+          icon={
+            <MaterialIcon
+              name="image-outline"
+              size={35}
+              color={colors.SECONDARY}
+            />
+          }
+          btnTitle="Select Poster"
+        />
+        <Selector
+          icon={
+            <MaterialIcon
+              name="file-music-outline"
+              size={35}
+              color={colors.SECONDARY}
+            />
+          }
+          btnTitle="Select Audio"
+          style={{marginLeft: 20}}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    padding: 10,
+  },
+  fileSelectorContainer: {
+    flexDirection: 'row',
+  },
 });
 
 export default Upload;
